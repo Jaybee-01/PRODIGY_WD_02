a19a88dae3c9e96b5125d6f2391f3ca3afd60f6e
Jaybee <emmnanueljoshua348@gmail.com>
2025-05-08 13:31:38 +0100

    My stop watch application

---
+++ scripts.js
@@ -0,0 +1,43 @@
+let [minutes, seconds, milliseconds] = [0, 0, 0];
+let timerRef = document.getElementById("display");
+let interval = null;
+let isRunning = false;
+
+function updateDisplay() {
+  let m = minutes < 10 ? `0${minutes}` : minutes;
+  let s = seconds < 10 ? `0${seconds}` : seconds;
+  let ms = milliseconds < 10 ? `0${milliseconds}` : milliseconds;
+  timerRef.innerHTML = `${m}:${s}:${ms}`;
+}
+
+function start() {
+  if (!isRunning) {
+    interval = setInterval(run, 10);
+    isRunning = true;
+  }
+}
+
+function run() {
+  milliseconds++;
+  if (milliseconds === 100) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+    }
+  }
+  updateDisplay();
+}
+
+function pause() {
+  clearInterval(interval);
+  isRunning = false;
+}
+
+function reset() {
+  clearInterval(interval);
+  [minutes, seconds, milliseconds] = [0, 0, 0];
+  isRunning = false;
+  updateDisplay();
+}
